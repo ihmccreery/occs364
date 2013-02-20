b0deab60d9f858db3384b6ad011a559859565910
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2013-02-20 14:43:28 -0500

    fixed manhattanDistance

--- puzzle8/search.py
+++ puzzle8/search.py
@@ -13,13 +13,13 @@ def numWrongTiles(state):
 	return numWrongTiles
 
 def manhattanDistance(state):
-	distance = 0
-	for x in range(9):
-		tile = puzzle8.getTile(state, x)
-		y = xyposition(state, tile)
-		distance += math.fabs(puzzle8.xylocation(x)[0] - puzzle8.xylocation(y)[0])
-		distance += math.fabs(puzzle8.xylocation(x)[1] - puzzle8.xylocation(y)[1])
-	return distance
+	manhattanDistance = 0
+	for i in range(9):
+		tile = puzzle8.getTile(state, i)
+		j = xyposition(puzzle8.solution(), tile)
+		manhattanDistance += math.fabs(puzzle8.xylocation(i)[0] - puzzle8.xylocation(j)[0])
+		manhattanDistance += math.fabs(puzzle8.xylocation(i)[1] - puzzle8.xylocation(j)[1])
+	return manhattanDistance
 
 def xyposition(state, tile):
 	for i in range(9):
