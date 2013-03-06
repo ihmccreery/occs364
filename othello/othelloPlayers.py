8c2c18c182c7f41245871c43d938abfd2379dfc5
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2013-03-05 23:16:15 -0500

    alphabeta and first pass at heuristic done

--- othello/othelloPlayers.py
+++ othello/othelloPlayers.py
@@ -8,6 +8,11 @@ the tournament between players.
 
 Feel free to add additional methods or functions.'''
 
+white = 1
+black = -1
+
+multipliers = [4, 1, 3, 2, 2, 3, 1, 4]
+
 class HumanPlayer:
     '''Interactive player: prompts the user to make a move.'''
     def __init__(self,name,color):
@@ -35,15 +40,40 @@ class HumanPlayer:
         else:
             return move
 
+# bogusHVal = -1
+# def heuristic(board):
+#     global bogusHVal 
+#     bogusHVal += 1
+#     #print "bogusHVal returned: " + str(bogusHVal//4)
+#     return bogusHVal//4
+
 def heuristic(board):
     '''This very silly heuristic just adds up all the 1s, -1s, and 0s
     stored on the othello board.'''
+
+    # possibilities:
+    #   - corner pieces worth much more
+    #   - side pieces worth some more
+    #
+    #   - mod row / column index
+
+    sum = 0
+    for i in range(1,othelloBoard.size-1):
+        for j in range(1,othelloBoard.size-1):
+            sum += board.array[i][j]*multipliers[i-1]*multipliers[j-1]
+    return sum
+
+def finalHeuristic(board):
+    '''This just counts up who wins, and returns
+    +/-infty based on who has won'''
+
     sum = 0
     for i in range(1,othelloBoard.size-1):
         for j in range(1,othelloBoard.size-1):
             sum += board.array[i][j]
-    return sum
-    
+    if sum > 0:
+        return sum*100000
+        # return float("inf")
 
 class ComputerPlayer:
     '''Computer player: chooseMove is where the action is.'''
@@ -54,12 +84,85 @@ class ComputerPlayer:
         self.plies = plies
 
     def chooseMove(self,board):
-        '''This very silly player just returns the first legal move
-        that it finds.'''
-        for i in range(1,othelloBoard.size-1):
-            for j in range(1,othelloBoard.size-1):
-                bcopy = board.makeMove(i,j,self.color)
-                if bcopy:
-                    print 'Heuristic value = ',self.heuristic(bcopy)
-                    return (i,j)
-        return None
+        a = float("-inf")
+        b = float("inf")
+        '''alpha-beta pruning'''
+        if self.color == black:
+            #print "MIN PLAYER:"
+            minMove = None
+            for i in range(1,othelloBoard.size-1):
+                for j in range(1,othelloBoard.size-1):
+                    bcopy = board.makeMove(i,j,self.color)
+                    if bcopy:
+                        #print 'Min: trying position "' + str(i) + ", " + str(j) + '"'
+                        v = self.alphaBeta(bcopy,white,self.plies-1,a,b,False)
+                        if v < b:
+                            b = v
+                            minMove = (i,j)
+            return minMove
+        else:
+            #print "MAX PLAYER:"
+            maxMove = None
+            for i in range(1,othelloBoard.size-1):
+                for j in range(1,othelloBoard.size-1):
+                    bcopy = board.makeMove(i,j,self.color)
+                    if bcopy:
+                        #print 'Max: trying position "' + str(i) + ", " + str(j) + '"'
+                        v = self.alphaBeta(bcopy,black,self.plies-1,a,b,False)
+                        if v > a:
+                            a = v
+                            maxMove = (i,j)
+            return maxMove
+
+    def alphaBeta(self,board,color,plies,a,b,lastPass):
+        #print "a,b: " + str(a) + "," + str(b)
+        if (plies == 0):
+            return self.heuristic(board)
+        if color == black:
+            #print "MIN PLAYER:"
+            v = float("inf")
+            seenLegalMove = False
+            for i in range(1,othelloBoard.size-1):
+                for j in range(1,othelloBoard.size-1):
+                    bcopy = board.makeMove(i,j,color)
+                    if bcopy:
+                        #print 'Min: trying position "' + str(i) + ", " + str(j) + '"'
+                        seenLegalMove = True
+                        v = min(v,self.alphaBeta(bcopy,white,plies-1,a,b,False))
+                        if v <= a:
+                            #print "MIN is PRUNING! Because minV: " + str(v) + " is <= a:" + str(b)
+                            return v
+                        b = min(b,v)
+            if seenLegalMove:
+                return v
+            else:
+                if not lastPass:
+                    board.display()
+                    return self.alphaBeta(board,white,plies-1,a,b,True)
+                else:
+                    board.display()
+                    return finalHeuristic(board)
+        else:
+            #print "MAX PLAYER:"
+            v = float("-inf")
+            seenLegalMove = False
+            for i in range(1,othelloBoard.size-1):
+                for j in range(1,othelloBoard.size-1):
+                    bcopy = board.makeMove(i,j,color)
+                    if bcopy:
+                        #print 'Max: trying position "' + str(i) + ", " + str(j) + '"'
+                        seenLegalMove = True
+                        v = max(v,self.alphaBeta(bcopy,black,plies-1,a,b,False))
+                        if v >= b:
+                            #print "MAX is PRUNING! Because maxV: " + str(v) + " is >= b:" + str(b)
+                            return v
+                        a = max(a,v)
+            if seenLegalMove:
+                return v
+            else:
+                if not lastPass:
+                    board.display()
+                    return self.alphaBeta(board,black,plies-1,a,b,True)
+                else:
+                    board.display()
+                    return finalHeuristic(board)
